fbf38773c6f48fb979e0800fe923a25ef238c8a2
svecalex <svecalex@seznam.cz>
2024-11-28 19:26:54 +0100

    update

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 from utils.hwio import getList
 
 app = Flask(__name__)
@@ -11,8 +10,20 @@ def index():
 @app.route('/settings')
 def settings():
     devices = getList()  # Zavolání funkce getList pro získání zařízení
-    return render_template("settings2.html", devices=devices)
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    return render_template("settings.html", devices=devices)
 
-    
+@app.route('/rename', methods=['POST'])
+def rename_device():
+    device_id = request.form['device_id']
+    new_alias = request.form['new_alias']
+    subprocess.run(["bch", "node", "rename", device_id, new_alias])
+    return redirect(url_for('settings'))
+
+@app.route('/delete', methods=['POST'])
+def delete_device():
+    device_id = request.form['device_id']
+    subprocess.run(["bch", "node", "remove", device_id])
+    return redirect(url_for('settings'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
